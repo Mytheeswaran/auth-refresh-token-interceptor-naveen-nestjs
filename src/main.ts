da8b40896ca01a1d1c759d6b7163e286a2baef7d
Mytheeswaran <mytheeswaran93@gmail.com>
2024-02-09 00:22:54 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials:true,
+    origin:"http://localhost:8080",
+  });
+  
+  app.use(cookieParser())
+  await app.listen(4000);
+}
+bootstrap();
